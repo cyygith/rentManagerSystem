1b63465f425ebbf4868b8aef4f894cf55a8500db
770764225 <770764225@qq.com>
2020-10-23 22:08:40 +0800

    generator the rentReceipt modify

--- src/page/rent/rentBill/rentBill.vue
+++ src/page/rent/rentBill/rentBill.vue
@@ -166,6 +166,43 @@ export default {
             });	
         },
         toReceipt(item,value){
+            let id = this.form.id;
+            let param = new URLSearchParams();
+            param.append("id",id);
+            // let loading = this.$loading({lock:true,text:'保存中....',background:'rgba(0,0,0,0.5)'});
+            billApi.getPdf(param).then(x => {   //
+                console.log("finally here....");
+                console.dir(x);
+                // if (x.status == 200) {
+                    // vm.dialogExportOrder = false;
+                    const content = x;
+                    const blob = new Blob([content])
+                    const fileName = '房组收据.pdf';  //自定义下载文件的名字
+                    if ('download' in document.createElement('a')) { // 非IE下载
+                        const elink = document.createElement('a');
+                        elink.download = fileName;
+                        elink.style.display = 'none';
+                        elink.href = URL.createObjectURL(blob);
+                        document.body.appendChild(elink);
+                        elink.click();
+                        URL.revokeObjectURL(elink.href); // 释放URL 对象
+                        document.body.removeChild(elink);
+                    } else { // IE10+下载
+                        navigator.msSaveBlob(blob, fileName);
+                    }
+                // }
+            }).catch(function(error){
+                console.log(error);
+                console.dir(error);
+            	// console.log(error.response)   //可获取错误的返回信息
+                // if (error.response.status==400) {
+                    
+                // }
+            }).finally(() => {
+            
+            });	
+        },
+        toReceipt1(item,value){
             // this.$router.push({path:'rentReceipt',query:{id:this.form.id}});
             let param = {
                 id:this.form.id,
